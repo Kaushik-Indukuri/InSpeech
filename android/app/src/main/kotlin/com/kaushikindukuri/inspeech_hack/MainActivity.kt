cf5374175d4e2d710a7fe4f579e8eecf53f083ab
Kaushik-Indukuri <49111141+Kaushik-Indukuri@users.noreply.github.com>
2021-03-20 19:20:14 -0700

    first commit

---
+++ android/app/src/main/kotlin/com/kaushikindukuri/inspeech_hack/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.kaushikindukuri.inspeech_hack
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:inspeech/screens/home.dart';
+import 'package:inspeech/services/helperfunctions.dart';
+import 'package:flutter/material.dart';
+import 'onboarding/welcome.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool userIsLoggedIn = false;
+
+  @override
+  void initState() {
+    getLoggedInState();
+    super.initState();
+  }
+
+  getLoggedInState() async {
+    await HelperFunctions.getUserLoggedInSharedPreference().then((val) {
+      setState(() {
+        userIsLoggedIn = val;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Home(),
+    );
+  }
+}
