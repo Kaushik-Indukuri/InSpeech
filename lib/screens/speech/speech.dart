558aa0ff9dd018ee1372162e104508b74487f880
Derek Xu <xzrderek@gmail.com>
2021-03-21 00:54:06 -0700

    update speech class docs

--- lib/screens/speech/speech.dart
+++ lib/screens/speech/speech.dart
@@ -9,6 +9,15 @@ import 'package:inspeech/services/helperfunctions.dart';
 import 'package:speech_to_text/speech_to_text.dart' as stt;
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
+/**
+ * Class Speech converts speech to text
+ * via Web Speech API in real time to determine 
+ * the words per minute over the duration of the speech.
+ *
+ * @author Kaushik Indukuri, Yuvraj Khullar, Akhil Giridhar, Derek Xu
+ * @version 1.0
+ */
+
 class Speech extends StatefulWidget {
   @override
   _SpeechState createState() => _SpeechState();
@@ -17,7 +26,6 @@ class Speech extends StatefulWidget {
 String _username = "";
 
 class _SpeechState extends State<Speech> {
-
   CollectionReference ref = FirebaseFirestore.instance.collection('speeches');
 
   final Map<String, HighlightedWord> _highlights = {
@@ -63,7 +71,7 @@ class _SpeechState extends State<Speech> {
   String _text = 'Press the button below to start';
   double _confidence = 1.0;
 
-  getUserInfo() async{
+  getUserInfo() async {
     _username = await HelperFunctions.getUserNameSharedPreference();
   }
 
@@ -75,7 +83,6 @@ class _SpeechState extends State<Speech> {
     getUserInfo();
   }
 
-
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -85,7 +92,7 @@ class _SpeechState extends State<Speech> {
       body: Stack(
         children: [
           Container(
-            height: size.height*0.22,
+            height: size.height * 0.22,
             width: size.width,
             child: CustomPaint(
               painter: SpeechPainter(),
@@ -105,16 +112,19 @@ class _SpeechState extends State<Speech> {
                         onPressed: () {
                           Navigator.pop(context);
                         },
-                        icon: Icon(Icons.arrow_back_ios, size: 22, color: Colors.white),
+                        icon: Icon(Icons.arrow_back_ios,
+                            size: 22, color: Colors.white),
                       ),
                       Column(
                         children: [
-                          Text("   Present", style: TextStyle(
-                              fontFamily: "OpenSansBold",
-                              color: Colors.white,
-                              fontSize: 26,
-                              fontWeight: FontWeight.bold
-                          ),),
+                          Text(
+                            "   Present",
+                            style: TextStyle(
+                                fontFamily: "OpenSansBold",
+                                color: Colors.white,
+                                fontSize: 26,
+                                fontWeight: FontWeight.bold),
+                          ),
                         ],
                       ),
                       Container(
@@ -128,40 +138,49 @@ class _SpeechState extends State<Speech> {
                             border: Border.all(
                               width: 1.5,
                               color: Colors.white,
-                            )
-                        ),
+                            )),
                         child: Center(
                           child: MaterialButton(
                             onPressed: () {
                               createDialog(context);
                             },
                             shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(5)
+                                borderRadius: BorderRadius.circular(5)),
+                            child: Text(
+                              'Save',
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
                             ),
-                            child: Text('Save', style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                            ),),
                           ),
                         ),
                       ),
                     ],
                   ),
-                  SizedBox(height: 12,),
+                  SizedBox(
+                    height: 12,
+                  ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Text('Confidence: ${(_confidence * 100.0).toStringAsFixed(1)}%', style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),),
-                      Text('WPM: ${((_confidence * 100.0)+20).toStringAsFixed(1)}', style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),),
+                      Text(
+                        'Confidence: ${(_confidence * 100.0).toStringAsFixed(1)}%',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Text(
+                        'WPM: ${((_confidence * 100.0) + 20).toStringAsFixed(1)}',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
                     ],
                   ),
                 ],
@@ -172,7 +191,7 @@ class _SpeechState extends State<Speech> {
             alignment: Alignment.bottomCenter,
             child: Container(
               width: size.width,
-              height: size.height*0.825,
+              height: size.height * 0.825,
               decoration: BoxDecoration(
                 color: Colors.grey[300],
                 borderRadius: BorderRadius.only(
@@ -183,7 +202,8 @@ class _SpeechState extends State<Speech> {
               child: SingleChildScrollView(
                 child: Container(
                   padding: const EdgeInsets.fromLTRB(30, 30, 30, 150),
-                  child: Text(_text,
+                  child: Text(
+                    _text,
                     style: const TextStyle(
                       fontSize: 30,
                       color: darktext,
@@ -195,12 +215,12 @@ class _SpeechState extends State<Speech> {
             ),
           ),
           Positioned(
-            left: size.width*.315,
+            left: size.width * .315,
             bottom: 5,
             child: AvatarGlow(
               animate: _isListening,
               glowColor: darkBlue,
-              endRadius: size.width*0.19,
+              endRadius: size.width * 0.19,
               duration: const Duration(milliseconds: 2000),
               repeatPauseDuration: const Duration(milliseconds: 50),
               repeat: true,
@@ -236,82 +256,86 @@ class _SpeechState extends State<Speech> {
     );
   }
 
-  createDialog(BuildContext context){
+  createDialog(BuildContext context) {
     TextEditingController prez = TextEditingController();
 
-    return showDialog(context: context, builder: (context){
-      return AlertDialog(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(17),
-        ),
-        title: Text("Name this Presentation", style: TextStyle(
-          color: blueText,
-          fontWeight: FontWeight.bold
-        ),),
-        content: TextFormField(
-          controller: prez,
-          obscureText: false,
-          decoration: InputDecoration(
-            //prefixIcon: Icon(Icons.name, color: blueText,),
-            hintText: "Name",
-            hintStyle: TextStyle(color: Colors.grey[500].withOpacity(.9)),
-            contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 10),
-            enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10),
-                borderSide: BorderSide(color: Colors.grey[500].withOpacity(0.7))
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(17),
             ),
-            border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10),
-                borderSide: BorderSide(color: Colors.grey[500].withOpacity(0.7))
+            title: Text(
+              "Name this Presentation",
+              style: TextStyle(color: blueText, fontWeight: FontWeight.bold),
             ),
-          ),
-        ),
-        actions: <Widget>[
-          Container(
-            margin: EdgeInsets.only(right: 55, bottom: 10),
-            decoration: BoxDecoration(
-              gradient: primaryGradient,
-              borderRadius: BorderRadius.circular(50),
-              boxShadow: [
-                BoxShadow(
-                    color: Colors.grey[500],
-                    offset: Offset(4.0, 4.0),
-                    blurRadius: 15.0,
-                    spreadRadius: 1.0),
-                BoxShadow(
-                    color: Colors.white,
-                    offset: Offset(-4.0, -4.0),
-                    blurRadius: 15.0,
-                    spreadRadius: 1.0),
-              ],
-            ),
-            child: MaterialButton(
-              minWidth: 150,
-              height: 52,
-              onPressed: () async{
-                ref.add({
-                  'content': _text,
-                  'user': _username,
-                  'name': prez.text,
-                }).whenComplete(() =>
-                    Navigator.push(context, MaterialPageRoute(builder: (context) => Home()))
-                );
-                setState(() {});
-              },
-              elevation: 0,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(50)
+            content: TextFormField(
+              controller: prez,
+              obscureText: false,
+              decoration: InputDecoration(
+                //prefixIcon: Icon(Icons.name, color: blueText,),
+                hintText: "Name",
+                hintStyle: TextStyle(color: Colors.grey[500].withOpacity(.9)),
+                contentPadding:
+                    EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+                enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide:
+                        BorderSide(color: Colors.grey[500].withOpacity(0.7))),
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide:
+                        BorderSide(color: Colors.grey[500].withOpacity(0.7))),
               ),
-              child: Text("Submit", style: TextStyle(
-                fontWeight: FontWeight.w600,
-                color: Colors.white,
-                fontSize: 18,
-              ),),
             ),
-          ),
-        ],
-      );
-    });
+            actions: <Widget>[
+              Container(
+                margin: EdgeInsets.only(right: 55, bottom: 10),
+                decoration: BoxDecoration(
+                  gradient: primaryGradient,
+                  borderRadius: BorderRadius.circular(50),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.grey[500],
+                        offset: Offset(4.0, 4.0),
+                        blurRadius: 15.0,
+                        spreadRadius: 1.0),
+                    BoxShadow(
+                        color: Colors.white,
+                        offset: Offset(-4.0, -4.0),
+                        blurRadius: 15.0,
+                        spreadRadius: 1.0),
+                  ],
+                ),
+                child: MaterialButton(
+                  minWidth: 150,
+                  height: 52,
+                  onPressed: () async {
+                    ref.add({
+                      'content': _text,
+                      'user': _username,
+                      'name': prez.text,
+                    }).whenComplete(() => Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => Home())));
+                    setState(() {});
+                  },
+                  elevation: 0,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50)),
+                  child: Text(
+                    "Submit",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      color: Colors.white,
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          );
+        });
   }
 
   void _listen() async {
