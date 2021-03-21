dc13fa5d5d1d361756cad4fc237945a3a22a94ec
Derek Xu <32891260+xzrderek@users.noreply.github.com>
2021-03-21 01:09:49 -0700

    Update LoadingAnimation.dart

--- lib/constants/LoadingAnimation.dart
+++ lib/constants/LoadingAnimation.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'colors.dart';
+
 /**
  * The Loading class is created to display a loading symbol while the pages load.
  *
  * @author Kaushik Indukuri, Yuvraj Khullar, Akhil Giridhar, Derek Xu
  * @version 1.0
 */
+
 class Loading extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
